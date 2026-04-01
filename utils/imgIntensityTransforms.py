f6e9c1c71f94f9d464322931a9140f4ca460b274
OGabiruuu <antunes.gabriel.345@gmail.com>
2026-04-01 11:22:49 -0300

    feat: Adds contrast_modulation and integrates this module with the test structure

--- utils/imgIntensityTransforms.py
+++ utils/imgIntensityTransforms.py
@@ -1,9 +1,6 @@
 import imageio.v3 as iio
 import numpy as np
 
-IMAGE_PATH = "../testImgs/ze.jpg"
-OUT_PATH = "../outImgs/out3.jpg"
-
 #-------------------------
 # Transformações
 #-------------------------
@@ -12,20 +9,40 @@ OUT_PATH = "../outImgs/out3.jpg"
 def invert_transform(light):
     return 255 - light
 
+
 # Diminui o dynamic range da imagem bruscamente (ou seja, a diferença entre claros e escuros)
 def log_transform(light):
     c = 255 / np.log(255 + 1)   # Fator de escala
     return np.uint8(c * np.log(np.float32(light) + 1))
+
 
 # Diminui o dynamic range mais suavemente
 def gamma_transform(light, gamma=2.2):
     c = 255 / (255 ** (1/gamma))
     return np.uint8(c * (np.float32(light) ** (1/gamma)))
 
+
+def contrast_modulation(light, entry_interval, exit_interval):
+    a, b = entry_interval[:]
+    c, d = exit_interval[:]
+
+    print(a, b)
+    print(c, d)
+
+    new_light = np.clip(((np.float32(light) - a) * ((d-c)/(b-a)) ), 0, 255)
+    return np.uint8(new_light)
+
+
 #-------------------------
 # Testes
 #-------------------------
 
-img = iio.imread(IMAGE_PATH)
-inv_img = gamma_transform(img)
-iio.imwrite(OUT_PATH, inv_img)
+if __name__ == "__main__":
+    IMAGE_PATH = "../testImgs/ze.jpg"
+    IMAGE_OUT = "../testImgs/out.jpg"
+
+    img = iio.imread(IMAGE_PATH)
+    inv_img = contrast_modulation(img, (0, 255), (200, 255))
+    #print(inv_img)
+
+    iio.imwrite(IMAGE_OUT, inv_img)
