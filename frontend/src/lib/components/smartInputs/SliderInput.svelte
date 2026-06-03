933043f45b23d190375ee8ba693a27b1eec9926f
OGabiruuu <antunes.gabriel.345@gmail.com>
2026-06-03 20:49:24 -0300

    feat: Adds styles for SliderInput

--- frontend/src/lib/components/smartInputs/SliderInput.svelte
+++ frontend/src/lib/components/smartInputs/SliderInput.svelte
@@ -26,7 +26,7 @@
 
 </script>
 
-<div>
+<div class="input-box">
     <label for="slider-inpt">{name} : {externalState}</label>
     <br>
     <input
@@ -39,3 +39,73 @@
         oninput={handleInputDebounced}
     >
 </div>
+
+<style>
+    .input-box {
+        padding: 8% 8% 8% 8%;
+        width: 100%;
+
+        border-radius: 5px;
+        background-color: var(--clr-outer-box);
+        border: solid 1px var(--clr-background);
+        transition: border-color 0.5s ease;
+    }
+
+    .input-box:hover {
+        border-color: var(--clr-dtl);
+    }
+
+    label {
+        display: block;
+        margin-bottom: 8%;
+
+        font-size: 1.2rem;
+    }
+
+    input {
+        margin: 2%;
+
+        text-align: center;
+        background-color: var(--clr-outer-box);
+
+        accent-color: var(--clr-dtl);
+        color: var(--clr-txt);
+        font-size: 1em;
+    }
+
+    input:hover {
+        cursor: grab;
+    }
+
+    input:hover:active {
+        cursor: grabbing;
+    }
+
+    /* Estilizando o botão (Thumb) no Firefox, Chromium e Safari */
+    input[type=range]::-moz-range-thumb {
+      background: var(--clr-dtl);
+      border: none;
+
+      accent-color: var(--clr-dtl);
+    }
+
+    input[type=range]::-webkit-slider-thumb {
+      background: var(--clr-dtl);
+      border: none;
+
+      accent-color: var(--clr-dtl);
+    }
+
+    /* Estilizando a barra (Track) no Firefox , Chromium e Safari*/
+    input[type=range]::-moz-range-track {
+      background-color: var(--clr-background);
+      accent-color: var(--clr-dtl);
+
+    }
+
+    input[type=range]::-webkit-slider-runnable-track {
+      background-color: var(--clr-background);
+      accent-color: var(--clr-dtl);
+
+    }
+</style>
